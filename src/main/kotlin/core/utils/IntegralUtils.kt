67a00ae0bcfc2cf66e387e7c07aa7a7d8e63374a
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-04-07 22:18:45 +0300

    fix: integral utils now static

--- src/main/kotlin/core/utils/IntegralUtils.kt
+++ src/main/kotlin/core/utils/IntegralUtils.kt
@@ -3,11 +3,9 @@ package core.utils
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class IntegralUtils {
-    companion object {
-        fun calcRungeRule(first: BigDecimal, second: BigDecimal, method: Int): BigDecimal {
-            return (second - first).divide(BigDecimal.valueOf(2)
-                .pow(method) - BigDecimal.ONE, 40, RoundingMode.HALF_UP)
-        }
+object IntegralUtils {
+    fun calcRungeRule(first: BigDecimal, second: BigDecimal, method: Int): BigDecimal {
+        return (second - first).divide(BigDecimal.valueOf(2)
+            .pow(method) - BigDecimal.ONE, 40, RoundingMode.HALF_UP)
     }
 }
