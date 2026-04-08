9b70d2517b0fb88d25468813a0aa190b82734475
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-04-08 05:40:48 +0300

    feat: added calculation button component

---
+++ src/main/kotlin/view/component/CalcButton.kt
@@ -0,0 +1,43 @@
+package view.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import view.utils.Colors
+import view.utils.Sizes
+
+@Composable
+fun calcButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier
+            .height(Sizes.middleHeight)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(Sizes.round),
+        color = Colors.accent,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize().padding(horizontal = Sizes.bigIndent)
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                letterSpacing = .5.sp
+            )
+        }
+    }
+}
