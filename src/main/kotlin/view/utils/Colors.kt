98afed9477fe574524cc3d1f08e2eee85c632da3
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-04-08 03:58:33 +0300

    feat: added colors utils

---
+++ src/main/kotlin/view/utils/Colors.kt
@@ -0,0 +1,12 @@
+package view.utils
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val sidebar = Color(0xFFF6F6F6)
+    val main = Color.White
+    val divider = Color(0xFFE5E5E5)
+    val accent = Color(0xFFFA2D48)
+    val textMain = Color(0xFF1D1D1F)
+    val textSecondary = Color(0xFF86868B)
+}
