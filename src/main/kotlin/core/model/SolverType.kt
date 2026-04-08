33c3125f6ce9efbe66781486e39726d39f8103dc
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-04-08 04:08:40 +0300

    fix: solvers names fixed

--- src/main/kotlin/core/model/SolverType.kt
+++ src/main/kotlin/core/model/SolverType.kt
@@ -1,9 +1,9 @@
 package core.model
 
 enum class SolverType(val label: String) {
-    LEFT_SQUARE("Метод левый прямоугольников"),
-    MIDDLE_SQUARE("Метод средний прямоугольников"),
-    RIGHT_SQUARE("Метод правый прямоугольников"),
+    LEFT_SQUARE("Метод левых прямоугольников"),
+    MIDDLE_SQUARE("Метод средних прямоугольников"),
+    RIGHT_SQUARE("Метод правых прямоугольников"),
     SIMPSON("Метод Симпсона"),
     TRAPEZE("Метод трапеций")
 }
